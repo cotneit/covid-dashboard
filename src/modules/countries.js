2d340421676f6b70da2866fd59fb012ad5aff5f3
RiSKail <egor29200125@gmail.com>
2020-12-18 17:21:30 +0300

    fix: fixed elint errors

--- src/modules/countries.js
+++ src/modules/countries.js
@@ -1,6 +1,4 @@
 import State from './state';
-import map from './map';
-import newChart from './covid-chart';
 
 // Источник: https://codepen.io/dcode-software/pen/zYGOrzK
 
@@ -45,7 +43,7 @@ export default function countriesTable(data, country, type) {
   data.forEach((elem, index) => {
     const tr = document.createElement('tr');
     tr.className = 'country-table__row';
-    
+
     if (elem.country === country) {
       tr.style.background = 'red';
     }
